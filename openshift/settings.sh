036bcbccb5c26d8e7056969ea88d78e84fc8cb9a
Wade Barnes <wade.barnes@shaw.ca>
2017-12-21 14:35:20 -0800

    Update setting file

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,11 +1,6 @@
 export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-devex-von-permitify}
-
-# The templates that should not have their GIT referances(uri and ref) over-ridden
-# Templates NOT in this list will have they GIT referances over-ridden
-# with the values of GIT_URI and GIT_REF
-export -a skip_git_overrides=("")
-export GIT_URI="https://github.com/bcgov/permitify.git"
-export GIT_REF="master"
+export GIT_URI=${GIT_URI:-"https://github.com/bcgov/permitify.git"}
+export GIT_REF=${GIT_REF:-"master"}
 
 # The project components
 # - They are all contained under the main OpenShift folder.
