cec87fa7393d1e33ec2143a431880d698e8295f8
Nicholas Rempel <nbrempel@gmail.com>
2018-01-15 15:32:31 -0800

    ignore date for now

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -21,7 +21,8 @@ $(function () {
 
     // Convert checkboxes to booleans
     $(this).find('input[type=date]').each((i, input) => {
-      data[input.name] = moment(input.value).unix()
+      // data[input.name] = moment(input.value).unix()
+      data[input.name] = ''
     })
 
     // Convert multi select to array
