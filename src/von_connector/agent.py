3fa75cb650b824ee03a1bbdb254e575b1f455ce4
Nicholas Rempel <nbrempel@gmail.com>
2018-01-19 13:21:13 -0800

    reference instance correctly

--- src/von_connector/agent.py
+++ src/von_connector/agent.py
@@ -83,7 +83,7 @@ class Holder:
 
     async def __aenter__(self):
         await self.pool.open()
-        await self.create_master_secret(uuid())
+        await self.instance.create_master_secret(uuid())
         return await self.instance.open()
 
     async def __aexit__(self, exc_type, exc_value, traceback):
