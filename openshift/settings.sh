e52544950e6d8ceb47d1226601aa67b2bd9ba51e
Wade Barnes <wade.barnes@shaw.ca>
2018-04-20 07:59:49 -0700

    Add enterprise wallet build and deployment configurations

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -6,11 +6,15 @@ export GIT_REF=${GIT_REF:-"master"}
 # - They are all contained under the main OpenShift folder.
 export components="."
 
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden with the values of GIT_URI and GIT_REF
+export skip_git_overrides="wallet-build.json"
+
 # The builds to be triggered after buildconfigs created (not auto-triggered)
 export builds=""
 
 # The images to be tagged after build
-export images="permitify"
+export images="permitify wallet"
 
 # The routes for the project
 export routes="bc-registries worksafe-bc onbis"
