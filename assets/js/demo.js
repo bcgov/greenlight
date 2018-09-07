514c2205d93be3559cac3512cfdf2e376d7c384a
Nicholas Rempel <nbrempel@gmail.com>
2018-09-06 17:07:15 -0700

    Add some hard-coded html to be later dynmically edited via jquery

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -2,11 +2,47 @@ $(() => {
   const params = new URL(location).searchParams;
   const topicId = params.get("topic");
 
+  // TODO: handle error case
   $.ajax({
     method: "GET",
-    url: `/bc-tob/api/topic/${topicId}`,
+    url: `/bc-tob/topic/${topicId}/formatted`,
     contentType: "application/json"
-  }).done(function(response) {
-    console.log(response);
-  });
+  })
+    .done(function(response) {
+      inflateUI(response);
+    })
+    .fail(response => {
+      try {
+        showError(response.responseJSON.detail);
+      } catch (err) {
+        console.error(err);
+        showError("Error");
+      }
+    });
 });
+
+function inflateUI(topic) {
+  // Inflate or info snippet
+  $.get("/assets/html/snippets/org-info.html").done(response => {
+    const snippet = $(response);
+    $("#content").html(snippet);
+
+    $.get("/assets/html/snippets/address.html").done(response => {
+      const snippet = $(response);
+      $("#address-content").html(snippet);
+    });
+
+    $.get("/assets/html/snippets/cert-panel.html").done(response => {
+      const snippet = $(response);
+      $("#address-content").html(snippet);
+    });
+  });
+}
+
+function showError(message) {
+  $.get("/assets/html/snippets/error.html").done(response => {
+    const snippet = $(response);
+    snippet.html(message);
+    $("#content").html(snippet);
+  });
+}
