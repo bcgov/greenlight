ae76b0f24797b0bfe706ea351a472dcac188a38c
Emiliano Suñé <emiliano.sune@quartech.com>
2018-12-11 13:56:27 -0800

    Added searchable dropdown for schemas in home component

--- dflow-demo/src/app/app.module.ts
+++ dflow-demo/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
-import { WorkflowStepComponent } from './components/workflow/workflow-step/workflow-step.component';
-import { FormsModule } from '@angular/forms';
 import { SearchInputComponent } from './components/search/search-input/search-input.component';
+import { WorkflowStepComponent } from './components/workflow/workflow-step/workflow-step.component';
 import { GeneralDataService } from './services/general-data.service';
 
 
@@ -33,7 +34,8 @@ import { GeneralDataService } from './services/general-data.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    NgSelectModule
   ],
   providers: [
     GeneralDataService
