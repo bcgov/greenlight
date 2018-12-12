14835e5f99c45981e0dc3b93f2c9c0297a2534d3
Emiliano Suñé <emiliano.sune@quartech.com>
2018-12-12 13:31:29 -0800

    Cleaned up and reorganized project structure.

--- src/von_connector/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'von_connector.apps.VonConnectorConfig'
--- src/von_connector/templatetags/index.py
+++
@@ -1,5 +0,0 @@
-from django.template.defaulttags import register
-
-@register.filter
-def index(sequence, position):
-    return sequence[position]
